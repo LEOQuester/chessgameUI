ffed9b21c989d5c9d9d13926574158d056b18e00
LEOQuester <induwaralakindu09@gmail.com>
2023-12-04 09:53:31 +0530

    update ui2

--- js/getGame.js
+++ js/getGame.js
@@ -3,30 +3,29 @@ let serial;
 let elo;
 
 function selectColor(selectedPlayer) {
-    player = selectedPlayer;
+  player = selectedPlayer;
 }
 
 function setSerialNumber(inputSerial) {
-    serial = inputSerial;
-    console.log(serial);
+  serial = inputSerial;
+  console.log(serial);
 }
 
 function setElo(inputElo) {
-    elo = inputElo;
+  elo = inputElo;
 }
-
 function showCollectedData() {
-    console.log(player, serial, elo);
+  console.log(player, serial, elo);
 }
 function submitData(event) {
-    event.preventDefault();
-    if (!player || !serial || !elo) {
-        alert("Please select your specifications before proceeding.");
-    } else {
-        console.log("validated");
-        // Send the data to the backend
-        // Once complete, then redirect
-        // window.location.href = "chessgame.html";
-    }
-}
+  event.preventDefault();
+  if (!player || !serial || !elo) {
+    alert("Please select your specifications before proceeding.");
+  } else {
+    console.log("validated");
+    // Send the data to the backend
 
+    // Once complete, then redirect
+    window.location.href = "chessgame.html";
+  }
+}
